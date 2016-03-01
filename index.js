a95b20a9fcd8480baeb81ed224e747e19066a561
thomasvaeth <thomas.vaeth@gmail.com>
2016-03-01 10:33:20 -0800

    Scrapped winning numbers

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var app = require('express')();
 var request = require('request');
 var $ = require('cheerio');
+var fs = require('fs');
 var port = process.env.PORT || 3000;
 
 app.get('/jackpot', function(req, res) {
@@ -14,6 +15,18 @@ app.get('/jackpot', function(req, res) {
 			jackpotArr.push(amount * 1000000 * 100);
 			res.send({amount: jackpotArr});
 		}
+	});
+});
+
+app.get('/numbers', function(req, res) {
+	request('http://www.powerball.com/powerball/winnums-text.txt', function(error, response, html) {
+		var recentNumbers = html.split('\n')[1];
+		var winningNumbers = recentNumbers.trim().split(' ').slice(1);
+		var winningArr = [];
+		winningNumbers.forEach(function(num) {
+			if (num !== '') winningArr.push(parseInt(num));
+		});
+		res.send({numbers: winningArr});
 	});
 });
 
